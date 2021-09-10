ad8cbf4ae576d74107a9c639d2d947850c83a3b8
monody157 <2737716714@qq.com>
2021-09-10 22:09:17 +0800

    清空工程，保留基本文件

--- src/packages/hooks/useUpdate.ts
+++
@@ -1,6 +0,0 @@
-import {useCallback, useState} from "react";
-
-export function useUpdate() {
-    const [count, setCount] = useState(0)
-    return useCallback(() => setCount(count + 1), [count])
-}
