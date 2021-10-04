f05147390f8cfebc198349688f23c98735093201
monody157 <2737716714@qq.com>
2021-10-04 21:40:01 +0800

    页面主体结构

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,67 @@
 import { useRef, useState } from 'react'
 import './app.scss'
 import { useCallbackRef } from './packages/hook/useCallbackRefs'
+import { ReactVisualEditor } from './packages/views/ReactVisualEditor'
 
 export default () => {
-    const [pos,setPos] = useState({
-        left: 0,
-        top:0
-    })
+    // const [pos,setPos] = useState({
+    //     left: 0,
+    //     top:0
+    // })
 
-    const moveDraggier = (()=>{
+    // const moveDraggier = (()=>{
 
-        const dragData = useRef({
-            startTop: 0,          // 拖拽开始时block的top值
-            startLeft: 0,         // 拖拽开始时block的left值
-            startX: 0,            // 拖拽开始时鼠标的X值
-            startY: 0             // 拖拽开始时鼠标的Y值
-        })
+    //     const dragData = useRef({
+    //         startTop: 0,          // 拖拽开始时block的top值
+    //         startLeft: 0,         // 拖拽开始时block的left值
+    //         startX: 0,            // 拖拽开始时鼠标的X值
+    //         startY: 0             // 拖拽开始时鼠标的Y值
+    //     })
 
-        const mouseDown = useCallbackRef((e:React.MouseEvent<HTMLDivElement>)=>{
-            document.addEventListener('mousemove',mouseMove),
-            document.addEventListener('mouseup',mouseUp),
-            dragData.current = {
-                startTop: pos.top,
-                startLeft: pos.left,
-                startX: e.clientX,
-                startY: e.clientY
-            }
-        })
-        const mouseMove = useCallbackRef((e:MouseEvent)=>{  // move函数一直不变，但是执行的是传入的最新的传入函数
-            const {startX,startY,startLeft,startTop} = dragData.current
-            const durX = e.clientX
-            const durY = e.clientY
-            setPos({
-                top: startTop + durY,
-                left: startLeft + durX
-            })
-            console.log(pos);
+    //     const mouseDown = useCallbackRef((e:React.MouseEvent<HTMLDivElement>)=>{
+    //         document.addEventListener('mousemove',mouseMove),
+    //         document.addEventListener('mouseup',mouseUp),
+    //         dragData.current = {
+    //             startTop: pos.top,
+    //             startLeft: pos.left,
+    //             startX: e.clientX,
+    //             startY: e.clientY
+    //         }
+    //     })
+    //     const mouseMove = useCallbackRef((e:MouseEvent)=>{  // move函数一直不变，但是执行的是传入的最新的传入函数
+    //         const {startX,startY,startLeft,startTop} = dragData.current
+    //         const durX = e.clientX
+    //         const durY = e.clientY
+    //         setPos({
+    //             top: startTop + durY,
+    //             left: startLeft + durX
+    //         })
+    //         console.log(pos);
             
-        })
-        const mouseUp = useCallbackRef(()=>{
-            document.removeEventListener('mousemove',mouseMove)
-            document.removeEventListener('mouseup',mouseUp)
-        })
-        return {mouseDown}
-    })()
+    //     })
+    //     const mouseUp = useCallbackRef(()=>{
+    //         document.removeEventListener('mousemove',mouseMove)
+    //         document.removeEventListener('mouseup',mouseUp)
+    //     })
+    //     return {mouseDown}
+    // })()
 
     return (
-        <div>
-            <div
-            style={{
-                height: '50px',
-                width: '50px',
-                backgroundColor: 'red',
-                position: 'relative',
-                top: `${pos.top}px`,
-                left: `${pos.left}px`,
-                display: 'inline-block'
-            }}
-            onMouseDown={moveDraggier.mouseDown}
-            >
-            </div>
-            </div>
-        
+        // <div>
+        //     <div
+        //     style={{
+        //         height: '50px',
+        //         width: '50px',
+        //         backgroundColor: 'red',
+        //         position: 'relative',
+        //         top: `${pos.top}px`,
+        //         left: `${pos.left}px`,
+        //         display: 'inline-block'
+        //     }}
+        //     onMouseDown={moveDraggier.mouseDown}
+        //     >
+        //     </div>
+        //     </div>
+        <ReactVisualEditor />
     )
 }
